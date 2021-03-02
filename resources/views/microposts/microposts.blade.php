54ceea9c4fb08a700aeb4da6c5fad18a0dd351a9
minoru.yoshino <mynois8@gmail.com>
2021-03-02 11:48:33 +0000

    favorite

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -14,7 +14,17 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div>
+                    <div class='row ml-0'>
+                        @if (Auth::user()->is_favorite($micropost->id))
+                            {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Unfavorite', ['class' => 'btn btn-success btn-sm']) !!}
+                            {!! Form::close() !!}
+                        @else
+                            {!! Form::open(['route' => ['favorites.favorite', $micropost->id], 'method' => 'post']) !!}
+                                {!! Form::submit('Favorite', ['class' => 'btn btn-light btn-sm']) !!}
+                            {!! Form::close() !!}
+                        @endif
+                        
                         @if (Auth::id() == $micropost->user_id)
                             {{-- 投稿削除ボタンのフォーム --}}
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
